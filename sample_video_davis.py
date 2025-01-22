c5b2a3c002aec9e66c389087a9716f412039f2c5
syarram24 <syarramcorp@gmail.com>
2025-01-22 12:37:02 +0530

    +1

--- sample_video_davis.py
+++ sample_video_davis.py
@@ -48,7 +48,8 @@ def main():
 
 
     transform = transforms.Compose([
-        transforms.CenterCrop((1024, 512)),  # Center crop to 1024x512 before resizing
+        transforms.Resize((960, 540)),
+        transforms.CenterCrop((960, 512)),  # Center crop to 1024x512 before resizing
         #transforms.Resize((960, 540)),  # Resize for VAE model compatibility
         transforms.ToTensor(),  # Convert to tensor
     ])
